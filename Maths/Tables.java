260a2b7bfa748406a840868082628da3296070b0
Muhammad Salman <sabsalman14@gmail.com>
2021-08-21 13:52:41 +0500

    Updated

--- Maths/Tables.java
+++ Maths/Tables.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Tables {
 	/*
 	 * This Program takes Input from the user and then prints its multiplications
-	 * upto 10. You can increase its multiplication upto whatever yu want. Just change the value in the while condition .
+	 * upto 10. You can increase its multiplication upto whatever you want. Just
+	 * change the value in the while condition .
 	 */
 	public static void main(String[] args) {
 		Scanner num = new Scanner(System.in);
@@ -11,7 +12,7 @@ public class Tables {
 		System.out.print("Enter the number to find its multiplications upto 10 : ");
 		int tabNum = num.nextInt();
 		int tabMul = 1;
-		while (tabMul <= 20) {			// Controlling the loop
+		while (tabMul <= 20) { // Controlling the loop
 
 			int result = tabNum * tabMul;
 			System.out.println(tabNum + " x " + tabMul + " = " + result);
