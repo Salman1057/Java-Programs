2b78b8e6fd8d9f1a9006ed98b8366fcf456a86cd
Muhammad Salman <sabsalman14@gmail.com>
2021-08-21 15:19:47 +0500

    Updated the code

--- Programming/Unicode.java
+++ Programming/Unicode.java
@@ -1,35 +1,24 @@
 import java.util.Scanner;
-public class Unicode{
-/*
-Java Program to assist the unicode conversions.
 
-*/
-	public static void main(String[] args){
-//Char Data Type to Unicode Conversion
-		Scanner inputFromUser = new Scanner(System.in);
-		System.out.println("\n");
-		System.out.println("Enter the Chracter to Convert into Unicode = " );
-		
-		char forget = inputFromUser.next().charAt(0);
-		System.out.println( "______________________________________________________________\n"+"\n\nAlphabetic Chracter you just entered =" + forget);
-		int converted = (int)forget;
-		System.out.println("Unicode of the entered Alphabet = " + converted);
-			System.out.println("\n\n---------------------------------------------");
-			System.out.println("---------------------------------------------\n\n");
+public class Unicode {
+	/*
+	 * 				Java Program to assist the unicode conversions.
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 														
 
+		System.out.print("\nEnter the Chracter to Convert into Unicode =    ");							// Char Data Type to Unicode Conversion
+		char enteredChar = input.next().charAt(0);
+		int convertedUnicode = (int) enteredChar;
+		System.out.println("Unicode of the Character " + enteredChar + " is = " + convertedUnicode );
 
+		System.out.println("\n---------------------------------------------\n");
 
-
-//Unicode to Char Data Type
-
-		Scanner inputFromUser1 = new Scanner(System.in);
-		System.out.println("Enter the Unicode to convert into Char Data Type = ");
-		
-		int forget1 = inputFromUser1.nextInt();
-		System.out.println("\n\nNumber you just entered = " + forget1);
-		char converted1= (char)forget1;
-		System.out.println("Char DataType of the Entered Number = " + converted1);
- 
+		System.out.print("Enter the Unicode to convert into Char Data Type =    "); 						// Unicode to Char Data Type
+		int enteredUnicode = input.nextInt();
+		char convertedChar = (char) enteredUnicode;
+		System.out.println("Char of the Unicode " + enteredUnicode + " is = " + convertedChar +"\n" );
+		input.close();
 
 	}
 
