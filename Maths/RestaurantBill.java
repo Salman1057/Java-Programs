e5b3d934d48422cee36a0717665cac0eb124ed19
Muhammad Salman <sabsalman14@gmail.com>
2021-08-21 09:37:21 +0500

    Updated the Code
    Input is taken from the user.

--- Maths/RestaurantBill.java
+++ Maths/RestaurantBill.java
@@ -1,19 +1,23 @@
-public class RestaurantBill{
+import java.util.Scanner;
+public class RestaurantBill {
+	/*
+	 * This Program takes charges and then calcualte the 6.75% tax on it and also
+	 * the tip which is 20% of the total charge (Including Taxes).
+	 * 
+	 */
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		System.out.print("Enter the Charges:  ");
+		int mealCharge = read.nextInt();
+		double tax = mealCharge * 6.75 / 100;
+		double totalCharge = mealCharge + tax;
+		double tip = totalCharge * 20 / 100;
 
-	public static void main(String[] args){
+		System.out.println("Meal Charge = " + mealCharge);
+		System.out.println("Tax on the Meal = " + tax);
+		System.out.println("Total Charge = " + totalCharge);
+		System.out.println("Tip on Meal = " + tip);
 
-		int mealCharge=3000;
-		double tax = mealCharge*6.75/100;
-		double totalCharge = mealCharge+tax;
-		double tip = totalCharge*20/100;
-	
-		System.out.println("Meal Charge = " +mealCharge);
-		System.out.println("Tax on the Meal = " +tax);
-		System.out.println("Total Charge = " +totalCharge);
-		System.out.println("Tip on Meal = " +tip);
-
-	
-		
 	}
 
 }
