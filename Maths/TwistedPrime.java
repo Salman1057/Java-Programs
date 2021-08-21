6db991b400f32e088962ee19a4e18bbdbf89a256
Salmanc_Student <72850566+Salman1057@users.noreply.github.com>
2021-08-21 16:22:30 +0500

    Update TwistedPrime.java

--- Maths/TwistedPrime.java
+++ Maths/TwistedPrime.java
@@ -9,44 +9,45 @@ public class TwistedPrime {
 	 * it prints that entered number is twisted prime.
 	 */
 	public static void main(String[] args) {
-
 		Scanner read = new Scanner(System.in);
+		
 		System.out.print("Enter the Number:   ");
 		int Num = read.nextInt();
 		int reverseNum = 0, numIncrement = 2, reverseNumIncrement = 2;
 		boolean resultNum = true, resultReverseNum = true;
 		int sqrtNum = (int) Math.sqrt(Num);
 
-		while (numIncrement <= sqrtNum) {
+		while (numIncrement <= sqrtNum) {				// Section to check for the Prime Number 
 			if (Num % numIncrement == 0) {
 				resultNum = false;
 			}
 			numIncrement++;
-
 		}
 		System.out.println("Is the Number " + Num + " Prime?? \n\t\t" + resultNum);
 
-		while (Num != 0) { /// Section to Reverse the Entered Number
+		while (Num != 0) { 						/// Section to Reverse the Entered Number
 			int remainder = Num % 10;
 			reverseNum = reverseNum * 10 + remainder;
 			Num /= 10;
 		}
+		
 		int sqrtReverseNum = (int) Math.sqrt(reverseNum);
-
-		while (reverseNumIncrement <= sqrtReverseNum) {
+		while (reverseNumIncrement <= sqrtReverseNum) {			// Section to check the reverse number for Twisted Prime
 			if (reverseNum % reverseNumIncrement == 0) {
 				resultReverseNum = false;
 			}
 			reverseNumIncrement++;
-
 		}
+		
 		System.out.println("\n\tThe reverse of the given number is: " + reverseNum + "\n");
 		System.out.println("Is the Number " + reverseNum + " Prime?? \n\t\t" + resultReverseNum);
 		read.close();
 		if (resultNum == true && resultReverseNum == true) {
 			System.out.println("Number is Twisted Prime");
-
+		}else{
+			System.out.println("Number is not Twisted Prime.");
 		}
+		
 	}
 
 }
