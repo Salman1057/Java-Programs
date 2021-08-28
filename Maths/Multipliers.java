72b452170602139764478a2b882d6ecf95f2f048
Muhammad Salman <sabsalman14@gmail.com>
2021-08-28 14:59:50 +0500

    Fixed a bug

--- Maths/Multipliers.java
+++ Maths/Multipliers.java
@@ -10,28 +10,24 @@ public class Multipliers {/*
         int result = read.nextInt();
         int tabNum = 2, tabMul = 1, tabMulCopy = 1, count = 0;
         boolean decision = true;
-        System.out.println("\n\n");
+        System.out.print("\n\n");
         while ((((tabNum * tabMul) < result)) || decision) {
             if ((tabNum * tabMul) >= result) {
                 if ((tabNum * tabMul) == result) {
-                    System.out.println("Number " + tabNum + " is being checked against Number " + result
-                            + ". Following result has been found.");
-                    System.out.println("\n\t\t\t" + tabNum + " x " + tabMul + " = " + result + "\n");
-                    count++;
-
-                    if (tabNum >= (result / 2)) {
-                        decision = false;
-                        System.out.println("\n\t\t\t\t\t !!!! PROGRAM ENDED !!!!\n\n");
-                    }
+                       System.out.println("Number " + tabNum + " is being checked against Number " + result + ". Following result has been found.");
+                       System.out.println("\n\t\t\t" + tabNum + " x " + tabMul + " = " + result + "\n");
+                        count++;
+                         if (tabNum >= (result / 2)) {
+                               decision = false;
+                               System.out.println("\n\t\t\t\t\t !!!! PROGRAM ENDED !!!!\n\n");
+                         }
                 }
-
                 tabMul = tabMulCopy;
                 tabNum++;
             }
             if (tabNum * tabMul >= result && count == 0) {
-                System.out.println("\n\t\t\t\t Number " + result + " has been checked against the numbers till "
+                System.out.print("\t\t Number " + result + " has been checked against the numbers till "
                         + (tabNum - 1) + ". No matches were found. " + result + " is a Prime Number.\n");
-                System.out.println("\n\t\t\t\t\t !!!! PROGRAM ENDED !!!!\n\n");
             }
             if (tabNum * tabMul >= result) {
                 decision = false;
